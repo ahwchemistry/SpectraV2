b5b3d2a6783b4bd19a0bbdb8d5d8c2d05953dfd7
alexandercentre <alexanderwilliams@Icarus.local>
2015-10-06 16:37:51 -0400

    Added thanks for help.

--- Points.java
+++ Points.java
@@ -9,46 +9,48 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.util.Stack;
 
+//Thanks to java2s.com for help figuring out how to use the Graphics Module.
+
 public class Points extends JPanel {
-	
+
 	public Stack<Double> xlines;
 
   public Points(Stack<Double> newstack){
-	  
-	  xlines = newstack;	
+
+	  xlines = newstack;
 	}
 
 public void paintComponent(Graphics g) {
     super.paintComponent(g);
- 
+
     Graphics2D g2d = (Graphics2D) g;
-    
+
     this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
-    
+
 
     g2d.setColor(Color.red);
 
     	Line2D xaxis = new Line2D.Double(0, 925, 1500, 925);
-    	
+
     	g2d.draw(xaxis);
-    	
+
     	for(int i2=0; i2 <= 14; i2++)
     	{
     		String currentstring = "" + i2;
-    		
+
     		g2d.drawString(currentstring, (14-i2)*100 ,950);
     	}
-    
+
     	while(!xlines.isEmpty())
     	{
     		System.out.println("Drawing" + xlines.peek());
-    		
+
     		Line2D currentline = new Line2D.Double( (14 - xlines.peek())*100, 925, (14 - xlines.peek())*100, 0);
-    		
+
     		xlines.pop();
-    		
+
     		g2d.draw(currentline);
-    	
+
     }
   }
 
@@ -56,7 +58,7 @@ public void paintComponent(Graphics g) {
 	Stack<Double> newstack = new Stack<Double>();
 	newstack.add(5.2);
 	newstack.add(6.3);
-	  
+
     Points points = new Points(newstack);
     JFrame frame = new JFrame("Points");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
